5bb213e44deed59cbc1444f9c59c0ec749e78c65
michalek990 <93988101+michalek990@users.noreply.github.com>
2023-12-18 11:51:48 +0100

    enable swagger to app

---
+++ src/main/java/com/example/finder/Config/SwaggerConfig.java
@@ -0,0 +1,11 @@
+package com.example.finder.Config;
+
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(info = @Info(title = "Docker Spring Rest API", version = "v1.0"))
+public class SwaggerConfig {
+}
